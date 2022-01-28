48a80f6fafb360ed38939411497a753602abd8be
masato1983 <50264750+masato1983@users.noreply.github.com>
2022-01-28 10:06:36 +0900

    build(postcss.config.js): Add plugins of postcss-clip-path-polyfill

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
         require('postcss-gap-properties'),
+        require('postcss-clip-path-polyfill'),
         require('postcss-viewport-height-correction'),
         require('postcss-100vh-fix'),
         require('postcss-flexbugs-fixes'),
